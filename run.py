d75401bb1e4619c7543298c56a0ef053b58d3aaa
zkb <18829036561@163.com>
2024-01-25 16:47:32 +0800

    Create a cross bidirectional linked list

--- run.py
+++ run.py
@@ -1,81 +1,132 @@
 import json
 from tools.node import Node
-from LocalModels.LocalLLM import Vicuna
-from LocalModels.Url_Test import Available_url
-from tools.func import is_align, merge_node, get_key_value
-from configs.config import score, score_threshold, n_generate_sample, value_map
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.prompts import PromptTemplate
-from tools.prompt import value_prompt
+from typing import List, Set
 
-url = Available_url().url
-print(url)
-llm = Vicuna(url)
-# prompt_template = PromptTemplate.from_template(template=value_prompt)
-# llm_chain = ConversationChain(llm=llm,
-#                               prompt=prompt_template,
-#                               memory=ConversationSummaryBufferMemory(llm=llm, max_token_limit=0,ai_prefix="AI:",human_prefix="问题："),
-#                               verbose=False)
-# res=llm(value_prompt.format(input="井号是井别"))
-# res=llm_chain.run({"input":"井号是井别"})
-# print(res)
 with open("test.json", "r", encoding='utf-8') as f:
     table_dict = json.load(f)
-print(table_dict)
-print(type(table_dict))
-root = Node(children=[])
+
+all_table_node: List[Node] = []
+table_rows, table_columns = 0, 0
 for one_row in table_dict['trs']:
     all_cells_of_row = one_row['tds']
     for cell in all_cells_of_row:
-        root.children.append(Node(colspan=cell['colspan'], rowspan=cell['rowspan'], context=cell['context']))
-    pass
-merge_log=[]
-while True:
-    max_score = 0
-    max_i, max_j = -1, -1
-    for i in range(len(root.children)):
-        for j in range(len(root.children)):
-            print("***********正在处理的节点：****************")
-            print("输出第i个节点：")
-            print("context:",root.children[i].context)
-            print("colspan:",root.children[i].colspan)
-            print("rowspan:",root.children[i].rowspan)
-            print("输出第j个节点：")
-            print("context:", root.children[j].context)
-            print("colspan:", root.children[j].colspan)
-            print("rowspan:", root.children[j].rowspan)
-            print("***************************")
-            if not is_align(root.children[i], root.children[j]):
-                continue
-            i_val, j_val = root.children[i].context, root.children[j].context
-            key,value = root.children[i], root.children[j]
-            llm_result=[]
-            for _ in range(n_generate_sample):
-                llm_val=llm(value_prompt.format(key=key.context,value=value.context))
-                if "是" in llm_val:
-                    llm_result.append("是")
-                if "否" in llm_val:
-                    llm_result.append('否')
-                if "可能" in llm_val:
-                    llm_result.append('可能')
-            value = sum(value * llm_result.count(name) for name, value in value_map.items())
-            print("value:",value)
-            if max_score <= value:
-                max_score = value
-                max_i, max_j = i, j
+        left_index, right_index = cell['colspan'][0], cell['colspan'][1]
+        up_index, down_index = cell['rowspan'][0], cell['rowspan'][1]
+        table_columns = right_index if table_columns < right_index else table_columns
+        table_rows = down_index if table_rows < down_index else table_rows
+        all_table_node.append(
+            Node(context=cell['context'], colspan=cell['colspan'], rowspan=cell['rowspan'],
+                 up_pointer=[None] * (table_rows + 1),
+                 down_pointer=[None] * (table_rows + 1),
+                 left_pointer=[None] * (table_columns + 1),
+                 right_pointer=[None] * (table_columns + 1)))
 
-    if max_score < score_threshold:
-        break
-    merged_colspan, merged_rowspan, merged_context = merge_node(root.children[max_i], root.children[max_j])
-    new_node = Node(colspan=merged_colspan, rowspan=merged_rowspan, context=merged_context)
-    root.children.pop(max_i)
-    root.children.pop(max_j)
-    root.children.append(new_node)
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    print("merged_context:",merged_context)
-    merge_log.append(merged_context)
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    print("merge_log:",merge_log)
+row_column_head = Node(context="", colspan=[0, 0], rowspan=[0, 0], up_pointer=[None], down_pointer=[None],
+                       left_pointer=[None], right_pointer=[None])
 
-print("merge_log:",merge_log)
+pre = row_column_head
+rows_head = []
+for i in range(table_rows):
+    row_head_i = Node(context="", colspan=[0, 0], rowspan=[i + 1, i + 1], up_pointer=[None] * (table_rows + 1),
+                      down_pointer=[None] * (table_rows + 1),
+                      left_pointer=[None] * (table_columns + 1), right_pointer=[None] * (table_columns + 1))
+    row_head_i.up_pointer[0] = pre
+    rows_head.append(row_head_i)
+    pre.down_pointer[0] = row_head_i
+    pre = row_head_i
+
+pre = row_column_head
+columns_head = []
+for i in range(table_columns):
+    column_head_i = Node(context="", colspan=[i + 1, i + 1], up_pointer=[None] * (table_rows + 1),
+                         down_pointer=[None] * (table_rows + 1),
+                         left_pointer=[None] * (table_columns + 1), right_pointer=[None] * (table_columns + 1))
+    column_head_i.left_pointer[0] = pre
+    columns_head.append(column_head_i)
+    pre.right_pointer[0] = column_head_i
+    pre = column_head_i
+
+for cell_node in all_table_node:
+
+    left_index, right_index = cell_node.colspan[0], cell_node.colspan[1]
+    up_index, down_index = cell_node.rowspan[0], cell_node.rowspan[1]
+
+    # 插入到水平双向链表
+    all_left_pre: Set[Node] = set(rows_head[up_index - 1:down_index])
+    tag = True
+    while tag:
+        tag = False
+        all_pre_temp: Set[Node] = set()
+        for i_pre in all_left_pre:
+            if not i_pre:
+                all_pre_temp.add(i_pre)
+            else:
+                i_pre_all_right_node: List[Node] = i_pre.right_pointer[up_index: down_index + 1]
+                for j_right_node in i_pre_all_right_node:
+                    if not j_right_node:
+                        all_pre_temp.add(i_pre)
+                    else:
+                        if j_right_node.colspan[1] < left_index:
+                            all_pre_temp.add(j_right_node)
+                            tag = True
+        if tag:
+            all_left_pre = all_pre_temp
+
+    all_right_next: Set[Node] = set()
+    for j_pre in all_left_pre:
+        all_right_next.update(j_pre.right_pointer[up_index: down_index + 1])
+
+    for j_pre in all_left_pre:
+        j_pre_up_index, j_pre_down_index = max(j_pre.rowspan[0], up_index), min(j_pre.rowspan[1], down_index)
+        cell_node.left_pointer[j_pre_up_index:j_pre_down_index + 1] = [j_pre] * (j_pre_down_index - j_pre_up_index + 1)
+        j_pre.right_pointer[j_pre_up_index:j_pre_down_index + 1] = [cell_node] * (j_pre_down_index - j_pre_up_index + 1)
+
+    for j_next in all_right_next:
+        if not j_next:
+            j_next_up_index, j_next_down_index = (max(j_next.rowspan[0], up_index),
+                                                  min(j_next.rowspan[1], down_index))
+            cell_node.right_pointer[j_next_up_index: j_next_down_index + 1] = [j_next] * (
+                    j_next_down_index - j_next_up_index + 1)
+            j_next.left_pointer[j_next_up_index: j_next_down_index + 1] = [cell_node] * (
+                    j_next_down_index - j_next_up_index + 1)
+
+    # 插入到垂直双向链表
+    all_up_pre: Set[Node] = set(columns_head[left_index - 1: right_index])
+    tag = True
+    while tag:
+        tag = False
+        all_pre_temp: Set[Node] = set()
+        for i_pre in all_up_pre:
+            if not i_pre:
+                all_pre_temp.add(i_pre)
+            else:
+                i_pre_all_down_node: List[Node] = i_pre.down_pointer[left_index: right_index + 1]
+                for j_down_node in i_pre_all_down_node:
+                    if not j_down_node:
+                        all_pre_temp.add(i_pre)
+                    else:
+                        if j_down_node.rowspan[1] < up_index:
+                            all_pre_temp.add(j_down_node)
+                            tag = True
+        if tag:
+            all_up_pre = all_pre_temp
+
+    all_down_next: Set[Node] = set()
+    for j_pre in all_up_pre:
+        all_down_next.update(j_pre.down_pointer[left_index: right_index + 1])
+
+    for j_pre in all_up_pre:
+        j_pre_left_index, j_pre_right_index = max(j_pre.colspan[0], left_index), min(j_pre.colspan[1], right_index)
+        cell_node.up_pointer[j_pre_left_index:j_pre_right_index + 1] = [j_pre] * (
+                j_pre_right_index - j_pre_left_index + 1)
+        j_pre.down_pointer[j_pre_left_index:j_pre_right_index + 1] = [cell_node] * (
+                j_pre_right_index - j_pre_left_index + 1)
+
+    for j_next in all_down_next:
+        if not j_next:
+            j_next_left_index, j_next_right_index = (max(j_next.colspan[0], left_index),
+                                                     min(j_next.colspan[1], right_index))
+            cell_node.down_pointer[j_next_left_index: j_next_right_index + 1] = [j_next] * (
+                    j_next_right_index - j_next_left_index + 1)
+            j_next.up_pointer[j_next_left_index: j_next_right_index + 1] = [cell_node] * (
+                    j_next_right_index - j_next_left_index + 1)
--- tools/node.py
+++ tools/node.py
@@ -1,10 +1,16 @@
+from typing import List
 class Node:
     """
     定义N叉树的节点
     """
 
-    def __init__(self, context=None, children=None, colspan=None, rowspan=None):
+    def __init__(self, context:str, colspan:List[int], rowspan:List[int], up_pointer:List, down_pointer:List,
+                 left_pointer:List, right_pointer:List):
         self.context = context
-        self.children = children
         self.colspan = colspan
         self.rowspan = rowspan
+
+        self.up_pointer=up_pointer
+        self.down_pointer=down_pointer
+        self.left_pointer=left_pointer
+        self.right_pointer=right_pointer
